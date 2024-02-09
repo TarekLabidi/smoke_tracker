fa36afa5082b689d0fdb7151c8d21ea36cbc234a
Tarek Labidi <63077008+obsidian55@users.noreply.github.com>
2024-02-09 20:49:25 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:data/smoked.dart';
-import 'package:first_try_for_a_good_app/home.dart';
-import 'package:first_try_for_a_good_app/palette.dart';
+import 'home.dart';
+import 'palette.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
